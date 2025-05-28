e472f25c80e63a24f5ab69884015d7c6ba4143b2
Luciano <lucianogallac7890@gmail.com>
2025-05-27 21:25:39 -0300

    Carpeta y TP5.1

---
+++ 05.1 Listas/TP N5.1.py
@@ -0,0 +1,66 @@
+
+#Ejercicio 1
+
+lista_range = list(range(0,101,4))
+print (lista_range)
+
+#Ejercicio 2
+
+Lista = ["Luciano","Dax","Bambi","Goliat","Neron"]
+Posicion_4 = Lista[3]
+print (Posicion_4)
+
+#Ejercicio 3 
+
+lista_vacia = []
+lista_vacia.append (1)
+lista_vacia.append ("Luciano")
+lista_vacia.append (False)
+print (lista_vacia)
+
+#Ejercicio 4
+
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales [3] = "oso"
+print (animales)
+
+#Ejercicio 5
+
+numeros = [8,15,3,22,7]
+numeros.remove(max(numeros))
+print (numeros)
+
+#En este ejercicio lo que hace el "".remove max" es buscar y remover el numero mas alto de la lista.
+
+#Ejercicio 6
+
+lista = list(range(10,31,5))
+print (lista[:2])
+
+#Ejercicio 7
+
+autos = ["sedan", "polo", "suran", "gol"]
+autos [1] = "saveiro"
+autos [2] = "audi"
+print (autos)
+
+#Ejercicio 8
+
+dobles = []
+dobles.append (10)
+dobles.append (20)
+dobles.append (30)
+print (dobles)
+
+#Ejercicio 9
+
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print (compras)
+
+#Ejercicio 10
+
+[15, True, [25.5, 57.9, 30.6], False]
